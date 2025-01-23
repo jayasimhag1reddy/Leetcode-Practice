e699df2a7b9d2a28292c8ee87a1d470aefda89c0
Jayasimha Reddy K <145401826+jayasimhag1reddy@users.noreply.github.com>
2025-01-22 19:38:44 -0800

    Time: 1 ms (82.03%), Space: 43.9 MB (14%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,24 +1,19 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans=new ArrayList<>();
-        helper(0,0,n,ans,"");
+        helper(0,0,n,"",ans);
         return ans;
     }
-    public void helper(int open,int close,int n,List<String> ans,String st){
+    public void helper(int open,int close,int n,String res,List<String> ans){
         if(close==n){
-            ans.add(st);
+            ans.add(res);
             return;
         }
         if(open<n){
-            helper(open+1,close,n,ans,st+"(");
+            helper(open+1,close,n,res+"(",ans);
         }
         if(close<open){
-            helper(open,close+1,n,ans,st+")");
+            helper(open,close+1,n,res+")",ans);
         }
     }
 }
-
-//   (
-// () ((
-// ()( (() (((
-// ()() ()((    
