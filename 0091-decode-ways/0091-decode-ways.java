d5a3a6565e36202c46c9a98a9f5bd411ff805ab9
Jayasimha Reddy K <145401826+jayasimhag1reddy@users.noreply.github.com>
2025-02-24 19:01:22 -0800

    Time: 1 ms (81.81%), Space: 42 MB (48.9%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,20 +1,19 @@
 class Solution {
     public int numDecodings(String s) {
-        int[] dp=new int[s.length()];
+        int[]dp=new int[s.length()];
         Arrays.fill(dp,-1);
         return helper(0,s,dp);
     }
     public int helper(int i,String s,int[] dp){
-        if(i>=s.length()){
-            return 1;
-        }
+        if(i>=s.length())return 1;
         if(s.charAt(i)=='0')return 0;
         if(dp[i]!=-1)return dp[i];
         int one=helper(i+1,s,dp);
         int two=0;
-        if(i<=s.length()-2 && Integer.parseInt(s.substring(i,i+2))<=26){
+        if(i+2<=s.length() && Integer.parseInt(s.substring(i,i+2))<27){
             two=helper(i+2,s,dp);
         }
         return dp[i]=one+two;
     }
+
 }
