b6fdd6878ae65fd753f61907199d6ebdf0b0c6cd
Jayasimha Reddy K <145401826+jayasimhag1reddy@users.noreply.github.com>
2025-03-04 19:03:28 -0800

    Time: 26 ms (31.89%), Space: 45.3 MB (24.9%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] dp=new int[coins.length+1][amount+1];
-        for(int[] ar:dp){
-            Arrays.fill(ar,-1);
-        }
-        if(helper(0,coins,amount,dp)>=100000)return -1;
-        return helper(0,coins,amount,dp);
+        int[][] dp=new int[coins.length][amount+1];
+        for(int[] ar:dp)Arrays.fill(ar,-1);
+        if(helper(coins,0,amount,dp)>=100001) return -1;
+        return helper(coins,0,amount,dp);
     }
-    public int helper(int i,int[] coins,int amt,int[][] dp){
+    public int helper(int[] coins,int i,int amount,int[][] dp){
         if(i==coins.length-1){
-            if(amt%coins[i]==0){
-                return amt/coins[i];
+            if(amount%coins[i]==0){
+                return amount/coins[i];
             }
             else{
                 return 100001;
             }
         }
-        if(dp[i][amt]!=-1)return dp[i][amt];
-        int np=helper(i+1,coins,amt,dp);
-        int p=100001;
-        if(coins[i]<=amt){
-            p=1+helper(i,coins,amt-coins[i],dp);
+        if(dp[i][amount]!=-1)return dp[i][amount];
+        int p=Integer.MAX_VALUE;
+        if(coins[i]<=amount){
+            p=1+helper(coins,i,amount-coins[i],dp);
         }
-        return dp[i][amt]=Math.min(p,np);
+        int np=helper(coins,i+1,amount,dp);
+        return dp[i][amount]=Math.min(np,p);
     }
 }
